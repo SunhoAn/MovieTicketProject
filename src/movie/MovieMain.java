35c3e6a5004726369419c43ad89fd7b7e39f8f83
SunhoAn <sho8478@naver.com>
2023-08-07 13:17:15 +0900

    올렸습니다

--- src/movie/MovieMain.java
+++ src/movie/MovieMain.java
@@ -7,7 +7,7 @@ public class MovieMain {
 		Movie m1 = new Movie("1917", 11000, 10, "인터넷");
 		Movie m2 = new Movie("주디", 15000, 25, "현장");
 		Movie m3 = new Movie("인비저블맨", 12500, 40, "쿠폰");
-       
+       System.out.println();
 		m1.titlePrint();
 		m1.rateCal();
 		m2.rateCal();
